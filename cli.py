6a9f9f7f84b4976b50aed6a24c0995adad88fc5e
Alex JPS <git@alexjps.com>
2023-06-12 13:38:50 -0700

    Add --week flag to manually set first date
    Flag -w or --week sets start week manually.
    Helpful when CSV contains tickets from old terms.
    Start date will always be a Monday now, even if automatically set.

--- cli.py
+++ cli.py
@@ -5,32 +5,75 @@ by Eric Edwards, Alex JPS
 
 The primary .py file form which the program should run.
 Parses user input via command-line arguments.
+Also performs basic input validation (e.g. formatting, valid files, etc.)
 Passes a dictionary with info to appropriate files or functions.
 """
 
+# libraries
 import argparse
+import sys
+import os
+from datetime import datetime, date, timedelta
+
+# files
 import plot
+
+def check_file(filename: str):
+    """
+    Check that the given filename exists and is a CSV file.
+    """
+    if not filename:
+        print("No file input provided", file=sys.stderr)
+        exit(1)
+    filename.strip()
+    if not (os.path.exists(filename)):
+        print("Always include filename as the last argument")
+        print(f"File {filename} not found", file=sys.stderr)
+        exit(1)
+    if (os.path.splitext(filename)[-1].lower()) != '.csv':
+            print("Always include filename as the last argument")
+            print(f"File {filename} is not a CSV file", file=sys.stderr)
+            exit(1)
+
+def check_date(date_text: str):
+    """
+    Checks that given string is in mm/dd/yyyy format.
+    Returns datetime object.
+    """
+    try:
+        date = datetime.strptime(date_text, "%m/%d/%Y")
+    except:
+        print(f"Date {date_text} does not follow mm/dd/yyyy format", file=sys.stderr)
+        exit(1)
+    return date
 
 
 def main():
+    """
+    Parse arguments, call basic input validation.
+    Call plot.py with args_dict.
+    """
+    input_filename = sys.argv.pop()
+    check_file(input_filename)
+    input_filename.strip()
+
     colors = ['white', 'black', 'gray', 'yellow', 'red', 'blue', 'green', 'brown', 'pink', 'orange', 'purple'] 
     
     parser = argparse.ArgumentParser()
     subparsers = parser.add_subparsers(help='sub_command help')
-
-    parser_set_info = subparsers.add_parser('set_info', help='the set_info command allows setting of the graph name')
     parser.add_argument('-n', '--name', type=str, help='Set the name of the plot.')
     parser.add_argument('-c', '--color', choices=colors, help='Set the color of the plot.')
-    parser.add_argument('-i', '--input', type=str, help='Set the filename from which CSV info is pulled.')
-
+    parser.add_argument('-w', '--week', type=str, help='Set week 1 to week of given mm/dd/yyyy (otherwise first ticket date used)')
     args = parser.parse_args()
-
     args_dict = vars(args)
 
-    # call plot tool
-    print(args_dict)
-    plot.ticketCountPerTerm(args_dict)
+    # custom values
+    args_dict['input'] = input_filename
+    if args_dict.get('week'):
+        args_dict['week'] = check_date(args_dict.get('week'))
 
+    # call plot tool
+    plot.ticketCountPerTerm(args_dict)
 
 if __name__ == "__main__":
     main()
--- plot.py
+++ plot.py
@@ -10,12 +10,13 @@ Report must at least contain Responsible Group and Modified.
 
 import csv  # used for reading the file
 import os
-from datetime import datetime, date  # used for sorting by modified date
+import sys
+from datetime import *  # used for sorting by modified date
 import matplotlib.pyplot as plt  # used for plotting pretty graphs
 
 
 def ticketCountPerTerm(args_dict):
-    filecheck(args_dict.get('input'))
+    check_fields(args_dict.get('input'))
     f = open(args_dict.get('input'), mode='r')
     csv_reader = csv.DictReader(f)
 
@@ -32,22 +33,24 @@ def ticketCountPerTerm(args_dict):
     # sort the tickets by the modified date
     classroomTickets.sort(key=lambda d: datetime.strptime(d['Modified'], "%m/%d/%Y %H:%M"))
 
-    ticketPerWeek = [0] * 11  # list of the counts of tickets per week
+    ticketsPerWeek = [0] * 11  # list of the counts of tickets per week
 
-    first_ticket = datetime.strptime(classroomTickets[0]['Modified'], "%m/%d/%Y %H:%M")
+    if args_dict.get('week'):
+        first_day = args_dict['week']
+    else:
+        first_day = datetime.strptime(classroomTickets[0]['Modified'], "%m/%d/%Y %H:%M")
+    first_day = get_monday(first_day)
+    print(f"the actual first date being used is {first_day}")
 
     for ticket in classroomTickets:
         # figure out which week the ticket is in
         date = datetime.strptime(ticket['Modified'], "%m/%d/%Y %H:%M")
-        delta = date - first_ticket
-        index = delta.days // 7
-        # incase you pulled a date a little too far
-        index = index - 1 if index >= 11 else index
-
+        delta = date - first_day
+        week = delta.days // 7
+        if week < 0:
+            continue
         # add to the count of whcih week its in
-        ticketPerWeek[index] += 1
-
-    print(ticketPerWeek)
+        ticketsPerWeek[week] += 1
 
     # Refactor this to a separate graphing function or file
     weeks = ["Week " + str(i + 1) for i in range(11)]  # list of week count for graph
@@ -56,9 +59,9 @@ def ticketCountPerTerm(args_dict):
     # initialize the graph
     fig, ax = plt.subplots(figsize=(10, 5))
     if args_dict.get('color'):
-        ax.bar(weeks, ticketPerWeek, color=args_dict.get('color'))
+        ax.bar(weeks, ticketsPerWeek, color=args_dict.get('color'))
     else:
-        ax.bar(weeks, ticketPerWeek, color='green')
+        ax.bar(weeks, ticketsPerWeek, color='green')
 
     # plt.xlabel("Weeks") # removed cause each bar is labeled Week (num)
     ax.set_ylabel("Count")
@@ -68,7 +71,7 @@ def ticketCountPerTerm(args_dict):
         ax.set_title("Number of Tickets Per Week")
 
     rect = ax.patches
-    for rect, c in zip(rect, ticketPerWeek):
+    for rect, c in zip(rect, ticketsPerWeek):
         # add the count to the graph
         height = rect.get_height()
         ax.text(
@@ -83,27 +86,25 @@ def ticketCountPerTerm(args_dict):
 
     plt.show()
 
-
-def filecheck(filename: str):
+def check_fields(filename):
     """
-    Check that the given filename exists and is a CSV file.
-    Check that the required fields are present.
-    Return opened file
+    Check that the required fields are present in file.
     """
-    if not filename:
-        print("No file input provided")
-        exit(1)
-    filename.strip()
-    if not (os.path.exists(filename)):
-        print("File input not found")
-        exit(1)
-    if (os.path.splitext(filename)[-1].lower()) != '.csv':
-            print("Not a CSV file")
-            exit(1)
-    file = open(filename, mode='r')
+    file = open(filename, mode="r")
     file_read = file.readlines()
     if not "Resp Group" in file_read[0]:
-        print("File input does not contain Resp Group")
+        print("File input does not contain Resp Group", file=sys.stderr)
+        exit(1)
     if not "Modified" in file_read[0]:
-        print("File input does not contain Modified")
+        print("File input does not contain Modified", file=stderr)
+        exit(1)
     file.close()
+
+def get_monday(date: datetime):
+    """
+    Given datetime, return the Monday of that week.
+    """
+    date = datetime.combine(date, time(0, 0))
+    while datetime.weekday(date):
+        date -= timedelta(days=1)
+    return date
